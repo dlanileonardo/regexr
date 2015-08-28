88fc6cf7db7f025e15b343bd6f510da4b0925cd9
Julio Makdisse Saito <saitodisse@gmail.com>
2015-08-28 18:04:11 -0300

    [azk] Azkfile.md and deploy sys

--- Azkfile.js
+++ Azkfile.js
@@ -40,4 +40,42 @@ systems({
       PORT: "8000",
     },
   },
+
+  deploy: {
+    image: {"docker": "azukiapp/deploy-digitalocean"},
+    mounts: {
+
+      // your files on remote machine
+      // will be on /home/git folder
+      "/azk/deploy/src":  path("."),
+
+      // will use your public key on server
+      // that way you can connect with:
+      // $ ssh git@REMOTE.IP
+      // $ bash
+      "/azk/deploy/.ssh": path("#{process.env.HOME}/.ssh")
+    },
+
+    // this is not a server
+    // just call with azk shell deploy
+    scalable: {"default": 0, "limit": 0},
+
+    envs: {
+      GIT_CHECKOUT_COMMIT_BRANCH_TAG: 'azkfile',
+      AZK_RESTART_COMMAND: 'azk restart -Rvv',
+      RUN_SETUP: 'true',
+      RUN_CONFIGURE: 'true',
+      RUN_DEPLOY: 'true',
+    }
+  },
+  "fast-deploy": {
+    extends: 'deploy',
+    envs: {
+      GIT_CHECKOUT_COMMIT_BRANCH_TAG: 'azkfile',
+      AZK_RESTART_COMMAND: 'azk restart -Rvv',
+      RUN_SETUP: 'false',
+      RUN_CONFIGURE: 'false',
+      RUN_DEPLOY: 'true',
+    }
+  },
 });
